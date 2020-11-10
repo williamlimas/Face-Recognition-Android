15aabac9f7e65ab72e4d83a54260084b61eaacd6
malik-anhar <malik-anhar@users.noreply.github.com>
2020-11-10 15:12:35 +0700

    Added custom liveness detection using custom design

---
+++ app/src/main/java/com/example/facerecognitiontflite/livenessfirebase/LivenessApp.java
@@ -0,0 +1,102 @@
+package com.example.facerecognitiontflite.livenessfirebase;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import com.example.facerecognitiontflite.R;
+
+import id.privy.livenessfirebasesdk.common.Constant;
+import id.privy.livenessfirebasesdk.entity.LivenessItem;
+import id.privy.livenessfirebasesdk.listener.PrivyCameraLivenessCallBackListener;
+
+public class LivenessApp {
+
+    private static PrivyCameraLivenessCallBackListener callback;
+
+    private Context context;
+
+    private Bundle bundle;
+
+    private LivenessApp(Context context, boolean isDebug, String successText, String instructions, String[] motionInstructions) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(Constant.Keys.IS_DEBUG, isDebug);
+        bundle.putString(Constant.Keys.SUCCESS_TEXT, successText);
+        bundle.putString(Constant.Keys.INSTRUCTION_TEXT, instructions);
+        bundle.putStringArray(Constant.Keys.MOTION_INSTRUCTIONS, motionInstructions);
+        this.context = context;
+        this.bundle = bundle;
+    }
+
+    public void start(PrivyCameraLivenessCallBackListener callback) {
+        LivenessApp.callback = callback;
+        Intent i = new Intent(context, SimpleLivenessActivity.class);
+        i.putExtras(bundle);
+        context.startActivity(i);
+    }
+
+    public static void setCameraResultData(Bitmap bitmap){
+        if (callback != null) {
+            if (bitmap != null) {
+                LivenessItem livenessItem = new LivenessItem();
+                livenessItem.setImageBitmap(bitmap);
+                callback.success(livenessItem);
+            }
+            else {
+                callback.failed(new Throwable("ImageBase Not Found"));
+            }
+        }
+        else {
+            callback.failed(new Throwable("Null Callback CameraLiveness"));
+        }
+    }
+
+    public static class Builder {
+
+        private Context context;
+
+        private boolean isDebug;
+
+        private String successText;
+
+        private String instructions;
+
+        private String leftInstruction, rightInstruction;
+
+        public Builder(Context context) {
+            this.context = context;
+            this.isDebug = false;
+            this.successText = context.getString(R.string.success_text);
+            this.successText = context.getString(R.string.instructions);
+            this.leftInstruction = context.getString(R.string.motion_instruction_left);
+            this.rightInstruction = context.getString(R.string.motion_instruction_right);
+        }
+
+        public LivenessApp.Builder setDebugMode(boolean isDebug) {
+            this.isDebug = isDebug;
+            return this;
+        }
+
+        public LivenessApp.Builder setSuccessText(String successText) {
+            this.successText = successText;
+            return this;
+        }
+
+        public LivenessApp.Builder setInstructions(String instructions) {
+            this.instructions = instructions;
+            return this;
+        }
+
+        public LivenessApp.Builder setMotionInstruction(String leftInstruction, String rightInstruction) {
+            this.leftInstruction = leftInstruction;
+            this.rightInstruction = rightInstruction;
+            return this;
+        }
+
+        public LivenessApp build() {
+            String[] motionInstructions = new String[]{leftInstruction, rightInstruction};
+            return new LivenessApp(context, isDebug, successText, instructions, motionInstructions);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/facerecognitiontflite/livenessfirebase/SimpleLivenessActivity.kt
@@ -0,0 +1,184 @@
+package com.example.facerecognitiontflite.livenessfirebase
+
+import android.Manifest
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.example.facerecognitiontflite.R
+import id.privy.livenessfirebasesdk.common.*
+import id.privy.livenessfirebasesdk.event.LivenessEventProvider
+import id.privy.livenessfirebasesdk.vision.VisionDetectionProcessor
+import id.privy.livenessfirebasesdk.vision.VisionDetectionProcessor.Motion
+import kotlinx.android.synthetic.main.activity_custom_liveness.*
+import java.io.IOException
+import java.util.*
+
+class SimpleLivenessActivity : AppCompatActivity() {
+
+    private val TAG = javaClass.simpleName
+
+    internal var preview: CameraSourcePreview? = null
+
+    internal var graphicOverlay: GraphicOverlay? = null
+
+    private var cameraSource: CameraSource? = null
+
+    private var visionDetectionProcessor: VisionDetectionProcessor? = null
+
+    private var success = false
+
+    private lateinit var successText: String
+
+    private var isDebug = false
+
+    private lateinit var motionInstructions: Array<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_liveness)
+
+        preview = findViewById(R.id.cameraPreview)
+        graphicOverlay = findViewById(R.id.faceOverlay)
+
+        if (intent.extras != null) {
+            val b = intent.extras!!
+            successText = b.getString(Constant.Keys.SUCCESS_TEXT, getString(R.string.success_text))
+            isDebug = b.getBoolean(Constant.Keys.IS_DEBUG, false)
+            instructions.text = b.getString(Constant.Keys.INSTRUCTION_TEXT, getString(R.string.instructions))
+            motionInstructions = b.getStringArray(Constant.Keys.MOTION_INSTRUCTIONS)!!
+        }
+
+        if (PermissionUtil.with(this).isCameraPermissionGranted) {
+            createCameraSource()
+            startHeadShakeChallenge()
+        }
+        else {
+            PermissionUtil.requestPermission(this, 1, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+        }
+
+        LivenessEventProvider.getEventLiveData().observe(this, Observer {
+            it?.let {
+                when {
+                    it.getType() == LivenessEventProvider.LivenessEvent.Type.HeadShake -> {
+                        onHeadShakeEvent()
+                    }
+
+                    it.getType() == LivenessEventProvider.LivenessEvent.Type.Default -> {
+                        onDefaultEvent()
+                    }
+                }
+            }
+        })
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        createCameraSource()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startCameraSource()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preview?.stop()
+        LivenessEventProvider.getEventLiveData().postValue(null)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraSource!!.release()
+    }
+
+    private fun createCameraSource() {
+        // If there's no existing cameraSource, create one.
+        if (cameraSource == null) {
+            cameraSource = CameraSource(this, graphicOverlay)
+            cameraSource!!.setFacing(CameraSource.CAMERA_FACING_FRONT)
+        }
+
+        val motion = Motion.values()[Random().nextInt(Motion.values().size)]
+
+        when (motion) {
+            Motion.Left -> {
+                motionInstruction.text = this.motionInstructions[0]
+            }
+
+            Motion.Right -> {
+                motionInstruction.text = this.motionInstructions[1]
+            }
+//
+//            Motion.Up -> {
+//                Toast.makeText(this, "Look Up", Toast.LENGTH_SHORT).show()
+//            }
+//
+//            Motion.Down -> {
+//                Toast.makeText(this, "Look Down", Toast.LENGTH_SHORT).show()
+//            }
+        }
+
+        visionDetectionProcessor = VisionDetectionProcessor()
+        visionDetectionProcessor!!.isSimpleLiveness(true, this, motion)
+        visionDetectionProcessor!!.isDebugMode(isDebug)
+
+        cameraSource!!.setMachineLearningFrameProcessor(visionDetectionProcessor)
+    }
+
+    private fun startCameraSource() {
+        if (cameraSource != null) {
+            try {
+                if (preview == null) {
+                    Log.d(TAG, "resume: Preview is null")
+                }
+                preview!!.start(cameraSource, graphicOverlay)
+            } catch (e: IOException) {
+                Log.e(TAG, "Unable to start camera source.", e)
+                cameraSource!!.release()
+                cameraSource = null
+            }
+        }
+    }
+
+    fun navigateBack(success: Boolean, bitmap: Bitmap?) {
+        if (bitmap != null) {
+            if (success) {
+                LivenessApp.setCameraResultData(BitmapUtils.processBitmap(bitmap))
+                finish()
+            }
+            else {
+                LivenessApp.setCameraResultData(null)
+                finish()
+            }
+        }
+    }
+
+    private fun startHeadShakeChallenge() {
+        visionDetectionProcessor!!.setVerificationStep(1)
+    }
+
+    private fun onHeadShakeEvent() {
+        if (!success) {
+            success = true
+            motionInstruction.text = successText
+
+            visionDetectionProcessor!!.setChallengeDone(true)
+        }
+    }
+
+    private fun onDefaultEvent() {
+        if (success) {
+            Handler().postDelayed({
+                cameraSource!!.takePicture(null, com.google.android.gms.vision.CameraSource.PictureCallback {
+                    navigateBack(true, BitmapFactory.decodeByteArray(it, 0, it.size))
+                })
+            }, 500)
+        }
+    }
+
+}
